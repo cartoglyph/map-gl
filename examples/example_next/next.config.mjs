a2668095c6b6dd348e3a98ba5edbbc0dfc6916c6
Nicholas <nicholaspaulsiebenaller@gmail.com>
2024-03-08 17:40:20 -0600

    Fix basePath in next config

--- examples/example_next/next.config.mjs
+++ examples/example_next/next.config.mjs
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   assetPrefix: isProd ? "https://dimapio.github.io/map-gl/" : undefined,
+  basePath: isProd ? "https://dimapio.github.io/map-gl/" : undefined,
 };
 
 export default nextConfig;
