24b1e0252959b996c0d826d7c04c47991c2e7755
Nicholas <nicholaspaulsiebenaller@gmail.com>
2024-03-08 17:34:37 -0600

    Rework node_env and asset dir

---
+++ examples/example_next/next.config.js
@@ -0,0 +1,9 @@
+const isProd = process.env.NODE_ENV === "production";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  assetPrefix: isProd ? "https://dimapio.github.io/map-gl/" : undefined,
+};
+
+export default nextConfig;
